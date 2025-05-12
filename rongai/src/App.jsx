8db31691cafa030426d740ee3e9e88ab5f79b275
Davies Gotosa <daviesgotosa10@gmail.com>
2025-05-12 22:59:04 +0200

    Created the App setup

---
+++ rongai/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl flex justify-center items-center font-bold h-100 ">Welcome, Rongai Safaris!</h1>
+    </div>
+  );
+}
+
+export default App;
